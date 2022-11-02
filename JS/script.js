25bb1135272e811725f8b4737a15f0207d16f21f
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-02 12:32:21 +0100

    starter materials

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+const {createApp} = Vue;
+
+const app = createApp({
+    data(){
+        return {
+            
+        }
+    },
+    methods:{
+        
+    },
+    computed:{
+        
+    },
+    mounted(){
+        console.log('in mounted');
+    },
+});
+app.mount('#app');
