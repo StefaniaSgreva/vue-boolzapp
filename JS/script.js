df0cbd01282c49fe6f8d58a39d01fe09d78be8ec
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-03 15:01:21 +0100

    added current date and tume

--- JS/script.js
+++ JS/script.js
@@ -10,17 +10,20 @@ const app = createApp({
                     avatar: '_1',
                     messages:[
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Ricordati di stendere i panni',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Tutto fatto!',
                             status: 'received'
                         }
@@ -33,17 +36,20 @@ const app = createApp({
                     avatar: '_2',
                     messages: [
                         {
-                            date: '20/03/2020 16:30:00',
+                            date: '20/03/2020',
+                            time: '12:00',
                             message: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
-                            date: '20/03/2020 16:30:55',
+                            date: '20/03/2020',
+                            time: '12:00',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
+                            date: '20/03/2020',
+                            time: '12:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
                         }
@@ -55,17 +61,20 @@ const app = createApp({
                     avatar: '_3',
                     messages: [
                         {
-                            date: '28/03/2020 10:10:40',
+                            date: '28/03/2020',
+                            time: '12:00',
                             message: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
+                            date: '28/03/2020',
+                            time: '12:00',
                             message: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            date: '28/03/2020 16:15:22',
+                            date: '28/03/2020',
+                            time: '12:00',
                             message: 'Ah scusa!',
                             status: 'received'
                         }
@@ -78,12 +87,14 @@ const app = createApp({
                     avatar: '_4',
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -95,12 +106,14 @@ const app = createApp({
                     avatar: '_5',
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Ricordati di chiamare la nonna',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Va bene, stasera la sento',
                             status: 'received'
                         }
@@ -112,17 +125,20 @@ const app = createApp({
                     avatar: '_6',
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Ciao Claudia, hai novità?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Non ancora',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Nessuna nuova, buona nuova',
                             status: 'sent'
                         }
@@ -134,12 +150,14 @@ const app = createApp({
                     avatar: '_7',
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received'
                         }
@@ -151,62 +169,74 @@ const app = createApp({
                     avatar: '_8',
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'OK!!',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'OK!!',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'OK!!',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:51:00',
+                            date: '10/01/2020',
+                            time: '12:00',
                             message: 'OK!!',
                             status: 'received'
                         }
@@ -224,20 +254,24 @@ const app = createApp({
         },
         sendMessage(){
             if(!this.newMessage) return;
-            const d = new Date();
-            let newDate = d.toDateString();
+            const current = new Date();
+            const date = current.getDate()+'-'+(current.getMonth()+1)+'-'+current.getFullYear();
+            const time = current.getHours() + ":" + current.getMinutes();
             const newSentMessage ={
-                date: newDate,
+                date: date,
+                time: time,
                 message: this.newMessage,
                 status: 'sent'
             }
             this.contacts[this.currentChat].messages.push(newSentMessage);
             this.newMessage = '';
             setTimeout(()=>{
-                const d = new Date();
-                let newDate = d.toDateString();
+                const current = new Date();
+                const date = current.getDate()+'/'+(current.getMonth()+1)+'/'+current.getFullYear();
+                const time = current.getHours() + ":" + current.getMinutes();
                 const newSentMessage ={
-                    date: newDate,
+                    date: date,
+                    time: time,
                     message: 'ok',
                     status: 'received'
                 }
